6274d82b2be448174a6a195619f36e0127f69332
gafisk <galihrestubaihq@gmail.com>
2024-07-10 07:50:00 +0700

    update index web

--- index.php
+++ index.php
@@ -141,33 +141,24 @@ WHERE users.ni_user = '$id'");
   <!-- Footer Start -->
   <div class="container-fluid bg-dark text-white mt-5 py-5 px-sm-3 px-md-5">
     <div class="row pt-5">
-      <div class="col-lg-7 col-md-6">
+      <div class="col-lg-7 col-md-12">
         <div class="row">
-          <div class="col-md-6 mb-5">
-            <h3 class="text-primary mb-4">Get In Touch</h3>
+          <div class="col-md-12 mb-5">
+            <h3 class="text-primary mb-4">Contact</h3>
             <p><i class="fa fa-map-marker-alt mr-2"></i>123 Street, New York, USA</p>
-            <p><i class="fa fa-phone-alt mr-2"></i>+012 345 67890</p>
-            <p><i class="fa fa-envelope mr-2"></i>info@example.com</p>
+            <p><i class="fa fa-phone-alt mr-2"></i>+62 856 4811 4 354 </p>
+            <p><i class="fa fa-envelope mr-2"></i>humas@smknkasiman.sch.id</p>
             <div class="d-flex justify-content-start mt-4">
-              <a class="btn btn-outline-light btn-social mr-2" href="#"><i class="fab fa-twitter"></i></a>
-              <a class="btn btn-outline-light btn-social mr-2" href="#"><i class="fab fa-facebook-f"></i></a>
+              <a class="btn btn-outline-light btn-social mr-2" href="http://smknkasiman.sch.id/"><i
+                  class="fas fa-globe"></i></a>
+              <!-- <a class="btn btn-outline-light btn-social mr-2" href="#"><i class="fab fa-facebook-f"></i></a>
               <a class="btn btn-outline-light btn-social mr-2" href="#"><i class="fab fa-linkedin-in"></i></a>
-              <a class="btn btn-outline-light btn-social" href="#"><i class="fab fa-instagram"></i></a>
-            </div>
-          </div>
-          <div class="col-md-6 mb-5">
-            <h3 class="text-primary mb-4">Quick Links</h3>
-            <div class="d-flex flex-column justify-content-start">
-              <a class="text-white mb-2" href="#"><i class="fa fa-angle-right mr-2"></i>Home</a>
-              <a class="text-white mb-2" href="#"><i class="fa fa-angle-right mr-2"></i>About Us</a>
-              <a class="text-white mb-2" href="#"><i class="fa fa-angle-right mr-2"></i>Our Services</a>
-              <a class="text-white mb-2" href="#"><i class="fa fa-angle-right mr-2"></i>Pricing Plan</a>
-              <a class="text-white" href="#"><i class="fa fa-angle-right mr-2"></i>Contact Us</a>
+              <a class="btn btn-outline-light btn-social" href="#"><i class="fab fa-instagram"></i></a> -->
             </div>
           </div>
         </div>
       </div>
-      <div class="col-lg-5 col-md-6 mb-5">
+      <!-- <div class="col-lg-5 col-md-12 mb-5">
         <h3 class="text-primary mb-4">Newsletter</h3>
         <p>Rebum labore lorem dolores kasd est, et ipsum amet et at kasd, ipsum sea tempor magna tempor. Accu
           kasd sed ea duo ipsum. Dolor duo eirmod sea justo no lorem est diam</p>
@@ -180,35 +171,17 @@ WHERE users.ni_user = '$id'");
             </div>
           </div>
         </div>
-      </div>
+      </div> -->
     </div>
   </div>
   <div class="container-fluid bg-dark text-white border-top py-4 px-sm-3 px-md-5"
     style="border-color: #3E3E4E !important;">
     <div class="row">
       <div class="col-lg-6 text-center text-md-left mb-3 mb-md-0">
-        <p class="m-0 text-white">&copy; <a href="#">Your Site Name</a>. All Rights Reserved.
-
-          <!--/*** This template is free as long as you keep the footer author’s credit link/attribution link/backlink. If you'd like to use the template without the footer author’s credit link/attribution link/backlink, you can purchase the Credit Removal License from "https://htmlcodex.com/credit-removal". Thank you for your support. ***/-->
-          Designed by <a href="https://htmlcodex.com">HTML Codex</a>
-          <br>Distributed By: <a href="https://themewagon.com" target="_blank">ThemeWagon</a>
+        <p class="m-0 text-white">&copy; <a href="#">SMKN Kasiman</a>. All Rights Reserved. | BETA
+          <!-- Designed by <a href="https://htmlcodex.com">HTML Codex</a>
+          <br>Distributed By: <a href="https://themewagon.com" target="_blank">ThemeWagon</a> -->
         </p>
-      </div>
-      <div class="col-lg-6 text-center text-md-right">
-        <ul class="nav d-inline-flex">
-          <li class="nav-item">
-            <a class="nav-link text-white py-0" href="#">Privacy</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link text-white py-0" href="#">Terms</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link text-white py-0" href="#">FAQs</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link text-white py-0" href="#">Help</a>
-          </li>
-        </ul>
       </div>
     </div>
   </div>
